d15d99ae7068c0749d3c960b443ab7622de12ba6
ikc20 <120470168+ikc20@users.noreply.github.com>
2023-12-18 23:50:50 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchWeatherData = async (city) => {
+    try {
+      const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      const response = await fetch(apiUrl);
+
+      if (response.ok) {
+        const data = await response.json();
+        setWeatherData(data);
+        setError(null); // Clear any previous errors
+      } else {
+        setError('Ville non trouvée. Veuillez réessayer.'); // Set error message
+      }
+    } catch (error) {
+      console.error('Une erreur s\'est produite : ', error);
+      setError('Erreur lors de la récupération des données météorologiques. Veuillez réessayer.');
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          const city = e.target.elements.city.value;
+          fetchWeatherData(city);
+        }}>
+          <label htmlFor="city">Ville : </label>
+          <input type="text" id="city" name="city" required />
+          <button type="submit">Obtenir la météo</button>
+        </form>
+
+        {weatherData && (
+          
+          
+          <div className="weather-info">
+            <h2>Météo pour {weatherData.name}, {weatherData.sys.country}</h2>
+            <p>Température : {weatherData.main.temp} °C</p>
+            <p>Description : {weatherData.weather[0].description}</p>
+            {weatherData.weather[0].icon && (
+              <img
+                src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+                alt="Conditions météorologiques"
+                className="weather-icon"
+              />
+              
+            )}
+          </div>
+          
+        )}
+
+        {error && (
+          <p className="error-message">{error}</p>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
